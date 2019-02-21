b0ab682d74ca793d8de068de20eae97f5932754c
Travis Recupero <31936622+travisrecupero@users.noreply.github.com>
2019-02-20 20:08:37 -0500

    Add files via upload

---
+++ lab05-travisrecupero-master/Zipper.java
@@ -0,0 +1,23 @@
+package lab05;
+import java.util.Arrays;
+
+public class Zipper{
+
+public static int[] zip(int[] arr1, int[] arr2){
+
+	if(arr1 == null || arr2 == null || arr1.length != arr2.length){
+		throw new IllegalArgumentException("bad input");
+	}
+
+	int size = arr1.length + arr2.length;
+	int[] temp = new int[size];
+	
+	int k = 0;
+	for(int i = 0; i < arr1.length; i++){
+			temp[k++] = arr1[i];
+			temp[k++] = arr2[i];
+	}
+	return temp;
+}
+
+}
